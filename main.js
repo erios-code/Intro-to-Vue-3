bcc1cff6c7c21e5a8344f8e6b3e66c2a4bba659f
Ervin Hernandez <64547345+erios-code@users.noreply.github.com>
2021-06-03 13:20:05 -0600

    Finalizacion Leccion 3

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            test:'test'
+            image: './assets/images/socks_green.jpg',
+            urlImage: 'https://i1.wp.com/hipertextual.com/wp-content/uploads/2016/06/hero-landscape-2736.jpg?fit=1200%2C702&ssl=1'
         }
     }
 })
